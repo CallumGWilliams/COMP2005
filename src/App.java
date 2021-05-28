58edc300cd4c4cd1539b8ed3221fe6ef19dae699
CallumGWilliams <57756266+CallumGWilliams@users.noreply.github.com>
2021-05-28 15:17:35 +0100

    implemented parse object

--- src/App.java
+++ src/App.java
@@ -1,14 +1,34 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
 
 public class App {
-    public Object getData() throws IOException, ParseException {
+    public Object getData() throws IOException, ParseException, org.json.simple.parser.ParseException {
         URL url = new URL("http://intelligent-social-robots-ws.com/restaurant-data.json");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         int responseCode = conn.getResponseCode();
-    return responseCode;
-    }
 
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            Object obj = new JSONParser().parse(new InputStreamReader(conn.getInputStream()));
+            JSONObject jo = (JSONObject) obj;
+
+            Integer id = (Integer) jo.get("id");
+            String n = (String) jo.get("name"); //returns null, has no field for "name" (NPE) (or any other keyword)
+
+            JSONArray a = (JSONArray) jo.get("restaurants"); //only way to return data using .get - returns json object
+
+            System.out.println(id); //prints null
+            System.out.println(n); //also prints null
+            System.out.println(a); //prints array of restaurants (inc. all dependant data)
+            return jo;
+
+        }
+        return responseCode;
+    }
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class TestClass {
 
     @Test
-    public void testCon() throws IOException, java.text.ParseException {
+    public void testCon() throws IOException, java.text.ParseException, ParseException {
         App app = new App();
         Object o = app.getData();
         System.out.println(o);
