7e43812ea7fa332415a84722f12d649b5ed05d24
CallumGWilliams <57756266+CallumGWilliams@users.noreply.github.com>
2021-05-28 15:12:25 +0100

    established http connection

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+
+public class App {
+    public Object getData() throws IOException, ParseException {
+        URL url = new URL("http://intelligent-social-robots-ws.com/restaurant-data.json");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        int responseCode = conn.getResponseCode();
+    return null;
+    }
+
+}
